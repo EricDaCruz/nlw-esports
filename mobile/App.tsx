1f5c275b301e2c86109325b20c03793382831271
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-09-12 22:58:25 -0300

    ⚛️ Mobile - Aula 2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,45 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-interface PropsButton {
-  title: string;
-}
-
-function Button ({title}: PropsButton) {
-  return(
-    <TouchableOpacity onPress={()=>console.log('Clicou no',title)}>
-      <Text style={styles.button}>
-        {title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+import {StatusBar} from 'react-native';
+import { Background } from './src/components/Background';
+import {useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black} from '@expo-google-fonts/inter'
+import { Home } from './src/screens/Home'
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_600SemiBold,  
+        Inter_700Bold,
+        Inter_900Black
+    })
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native!</Text>
-      <Button title="Botão 1"/>
-      <Button title="Botão 2"/>
-      <StatusBar style="auto" />
-    </View>
+      <Background>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Home /> : <Loading />}
+      </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title:{
-    fontSize: 22,
-    marginBottom: 20,
-  },
-  button:{
-    color: '#9e9e9e',
-    fontSize: 18,
-    marginBottom: 10,
-  }
-});
